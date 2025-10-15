77723d73610561e30e16b46d517a31dc229bf7a4
damzG <C00313475@setu.ie>
2025-10-15 17:51:03 +0100

    Adjusted Vet Class

--- lab7/Vet.java
+++ lab7/Vet.java
@@ -1,8 +1,12 @@
 public class Vet {
-    private String name;
+    final private String name;
 
     public Vet(String name){
         this.name = name;
+    }
+
+    public String getName(){
+        return name;
     }
 
     public void Vaccinate(Animal animal){
